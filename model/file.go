3c3f8a9f934c3787dafbd9aac20f28aee3e1c05f
Jakub Knejzlik <jakubknejzlik@Jakubs-MacBook-Pro.local>
2019-03-15 12:02:42 +0100

    Update file model to return file ID

--- model/file.go
+++ model/file.go
@@ -2,6 +2,7 @@ package model
 
 // File ...
 type File struct {
+	ID          string `json:"id"`
 	UID         string `json:"uid"`
 	Size        int64  `json:"size"`
 	ContentType string `json:"contentType"`
